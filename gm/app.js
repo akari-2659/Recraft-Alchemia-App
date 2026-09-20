f6f7e75449809ee5bccb9448852bed1233daa0e2
akari-2659 <zikkyousyananoka@gmail.com>
2026-09-20 14:11:54 +0900

    Add files via upload

--- gm/app.js
+++ gm/app.js
@@ -1,5 +1,5 @@
 (()=>{'use strict';
-const APP_VERSION='1.0.37';
+const APP_VERSION='1.0.40';
 const ACCOUNT=window.RA_ACCOUNT;
 const app=document.querySelector('#gmApp'),authError=document.querySelector('#authError');
 const sidebar=document.querySelector('#sidebar'),backdrop=document.querySelector('#backdrop'),mobileTitle=document.querySelector('#mobileTitle');
